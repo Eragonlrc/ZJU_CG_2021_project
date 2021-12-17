43fc646d242d65829facc9c324ca8313e8d96789
Eragonlrc <eragonlrc314@gmail.com>
2021-12-17 22:37:21 +0800

    first test for the whole project

---
+++ project/belt.cpp
@@ -0,0 +1,300 @@
+#include "belt.h"
+#include <math.h>
+static const float C = acos(-1) / 180;
+
+TexLoader Belt::texture(2);
+const float Belt::beltHeight = 0.05;
+const float Belt::beltSpeed = 0.002;
+const float Belt::radius[] = { 0.1, 0.2, 0.8, 0.9 };
+const float Belt::radius0 = 2 / acos(-1);
+const float Belt::height[] = { 0.2, 0.3, 0.4 };
+float Belt::beltMove = 0;
+
+Belt::Belt() = default;
+
+void Belt::init() {
+    texture.genTex();
+    texture.loadTex(0, "textures/iron.bmp");
+    texture.loadTex(1, "textures/belt.bmp");
+}
+
+void Belt::update() {
+    beltMove += beltSpeed;
+    if (beltMove >= 1) beltMove -= 1;
+}
+
+void Belt::pushPoint(int z, int x) {
+    points.push_back(std::pair<int, int>(z, x));
+}
+
+void Belt::draw() {
+    for (int i = 1; i < points.size() - 1; i++) {
+        bool rev = 0;
+        int from, to;
+        glPushMatrix();
+        if (points[i].first == points[i - 1].first) {
+            if (points[i].second > points[i - 1].second) from = 0;
+            else from = 2;
+        }
+        else {
+            if (points[i].first > points[i - 1].first) from = 1;
+            else from = 3;
+        }
+        if (points[i].first == points[i + 1].first) {
+            if (points[i].second > points[i + 1].second) to = 0;
+            else to = 2;
+        }
+        else {
+            if (points[i].first > points[i + 1].first) to = 1;
+            else to = 3;
+        }
+        glTranslatef(points[i].second, 0, points[i].first);
+        switch ((to - from + 4) % 4) {
+            case 3: case 1: {
+                glRotatef(-90 * from, 0, 1, 0);
+                if((to - from + 4) % 4 == 3) drawCornerCCW(0);
+                else drawCornerCW(0);
+                break;
+            }
+            case 2: {
+                glRotatef(-90 * from, 0, 1, 0);
+                drawStraight(0);
+                break;
+            }
+        }
+        glPopMatrix();
+    }
+}
+
+void Belt::drawStraight(bool rev) {
+    float _beltMove = rev ? 1 - beltMove : beltMove;
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+    glTranslatef(-0.5, 0, -0.5);
+    // frame
+    texture.bindTex(0);
+    glBegin(GL_QUADS);
+    // down
+    glTexCoord2f(radius[0], 0); glVertex3f(0, height[0], radius[0]);
+    glTexCoord2f(radius[3], 0); glVertex3f(0, height[0], radius[3]);
+    glTexCoord2f(radius[3], 1); glVertex3f(1, height[0], radius[3]);
+    glTexCoord2f(radius[0], 1); glVertex3f(1, height[0], radius[0]);
+    // up
+    for (int i = 0; i < 3; i++) {
+        glTexCoord2f(radius[i], 0); glVertex3f(0, height[i == 1 ? 1 : 2], radius[i]);
+        glTexCoord2f(radius[i+1], 0); glVertex3f(0, height[i == 1 ? 1 : 2], radius[i+1]);
+        glTexCoord2f(radius[i+1], 1); glVertex3f(1, height[i == 1 ? 1 : 2], radius[i+1]);
+        glTexCoord2f(radius[i], 1); glVertex3f(1, height[i == 1 ? 1 : 2], radius[i]);
+    }
+    // side
+    for (int i = 0; i < 4; i++) {
+        glTexCoord2f(height[2], 0); glVertex3f(0, height[2], radius[i]);
+        glTexCoord2f(height[2], 1); glVertex3f(1, height[2], radius[i]);
+        if (i == 0 || i == 3) {
+            glTexCoord2f(height[0], 1); glVertex3f(1, height[0], radius[i]);
+            glTexCoord2f(height[0], 0); glVertex3f(0, height[0], radius[i]);
+        }
+        else {
+            glTexCoord2f(height[1], 1); glVertex3f(1, height[1], radius[i]);
+            glTexCoord2f(height[1], 0); glVertex3f(0, height[1], radius[i]);
+        }
+    }
+    glEnd();
+
+    // belt
+    texture.bindTex(1);
+    glBegin(GL_QUADS);
+    // part 1
+    glTexCoord2f(radius[2], 1 - _beltMove); glVertex3f(1, height[1] + beltHeight, radius[2]);
+    glTexCoord2f(radius[1], 1 - _beltMove); glVertex3f(1, height[1] + beltHeight, radius[1]);
+    glTexCoord2f(radius[1], 0); glVertex3f(_beltMove, height[1] + beltHeight, radius[1]);
+    glTexCoord2f(radius[2], 0); glVertex3f(_beltMove, height[1] + beltHeight, radius[2]);
+    // part 2
+    glTexCoord2f(radius[2], 1); glVertex3f(_beltMove, height[1] + beltHeight, radius[2]);
+    glTexCoord2f(radius[1], 1); glVertex3f(_beltMove, height[1] + beltHeight, radius[1]);
+    glTexCoord2f(radius[1], 1 - _beltMove); glVertex3f(0, height[1] + beltHeight, radius[1]);
+    glTexCoord2f(radius[2], 1 - _beltMove); glVertex3f(0, height[1] + beltHeight, radius[2]);
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void Belt::drawCornerCW(bool rev) {
+    static const float deltaAngle = 0.1;
+    float _beltMove = rev ? (1 - beltMove) / radius0 / C : beltMove / radius0 / C;
+
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+    glTranslatef(-0.5, 0, -0.5);
+    // frame
+    texture.bindTex(0);
+    glBegin(GL_QUADS);
+    // down
+    for (float i = 0; i < 90; i += deltaAngle) {
+        float rad[] = { i * C, (i + deltaAngle) * C };
+        float seg[][2] = { {radius[0] * cos(rad[0]), radius[0] * sin(rad[0])},
+                           {radius[3] * cos(rad[0]), radius[3] * sin(rad[0])},
+                           {radius[3] * cos(rad[1]), radius[3] * sin(rad[1])},
+                           {radius[0] * cos(rad[1]), radius[0] * sin(rad[1])} };
+        float texseg[][2] = { {radius[0], radius0 * rad[0]},
+                              {radius[3], radius0 * rad[0]},
+                              {radius[3], radius0 * rad[1]},
+                              {radius[0], radius0 * rad[1]} };
+        for (int j = 0; j < 4; j++) {
+            glTexCoord2fv(texseg[j]);
+            glVertex3f(seg[j][1], height[0], seg[j][0]);
+        }
+    }
+    // up
+    for(int k = 0; k < 3; k++){
+        for (float i = 0; i < 90; i += deltaAngle) {
+            float rad[] = { i * C, (i + deltaAngle) * C };
+            float seg[][2] = { {radius[k] * cos(rad[0]), radius[k] * sin(rad[0])},
+                               {radius[k+1] * cos(rad[0]), radius[k+1] * sin(rad[0])},
+                               {radius[k+1] * cos(rad[1]), radius[k+1] * sin(rad[1])},
+                               {radius[k] * cos(rad[1]), radius[k] * sin(rad[1])} };
+            float texseg[][2] = { {radius[k], radius0 * rad[0]},
+                                  {radius[k+1], radius0 * rad[0]},
+                                  {radius[k+1], radius0 * rad[1]},
+                                  {radius[k], radius0 * rad[1]} };
+            for (int j = 0; j < 4; j++) {
+                glTexCoord2fv(texseg[j]);
+                glVertex3f(seg[j][1], height[k==1 ? 1 : 2], seg[j][0]);
+            }
+        }
+    }
+    // side
+    for (int k = 0; k < 4; k++) {
+        for (float i = 0; i < 90; i += deltaAngle) {
+            float rad[] = { i * C, (i + deltaAngle) * C };
+            float seg[][2] = { {radius[k] * cos(rad[0]), radius[k] * sin(rad[0])},
+                               {radius[k] * cos(rad[1]), radius[k] * sin(rad[1])} };
+            glTexCoord2f(height[2], radius[k] * rad[0] - int(radius[k] * rad[0])); glVertex3f(seg[0][1], height[2], seg[0][0]);
+            glTexCoord2f(height[2], radius[k] * rad[1] - int(radius[k] * rad[1])); glVertex3f(seg[1][1], height[2], seg[1][0]);
+            if (k == 0 || k == 3) {
+                glTexCoord2f(height[0], radius[k] * rad[1] - int(radius[k] * rad[1])); glVertex3f(seg[1][1], height[0], seg[1][0]);
+                glTexCoord2f(height[0], radius[k] * rad[0] - int(radius[k] * rad[0])); glVertex3f(seg[0][1], height[0], seg[0][0]);
+            }
+            else {
+                glTexCoord2f(height[1], radius[k] * rad[1] - int(radius[k] * rad[1])); glVertex3f(seg[1][1], height[1], seg[1][0]);
+                glTexCoord2f(height[1], radius[k] * rad[0] - int(radius[k] * rad[0])); glVertex3f(seg[0][1], height[1], seg[0][0]);
+            }
+        }
+    }
+    glEnd();
+
+    // belt
+    texture.bindTex(1);
+    glBegin(GL_QUADS);
+    for (float i = 0; i < 90; i += deltaAngle) {
+        float rad[] = { i * C, (i + deltaAngle) * C };
+        float texrad[2];
+        if (i >= _beltMove) texrad[0] = (i - _beltMove) * C, texrad[1] = (i + deltaAngle - _beltMove) * C;
+        else texrad[0] = (i + (90 - _beltMove)) * C, texrad[1] = (i + deltaAngle + (90 - _beltMove)) * C;
+        float seg[][2] = { {radius[1] * cos(rad[0]), radius[1] * sin(rad[0])},
+                           {radius[2] * cos(rad[0]), radius[2] * sin(rad[0])},
+                           {radius[2] * cos(rad[1]), radius[2] * sin(rad[1])},
+                           {radius[1] * cos(rad[1]), radius[1] * sin(rad[1])} };
+        float texseg[][2] = { {radius[1], radius0 * texrad[0]},
+                              {radius[2], radius0 * texrad[0]},
+                              {radius[2], radius0 * texrad[1]},
+                              {radius[1], radius0 * texrad[1]} };
+        for (int j = 0; j < 4; j++) {
+            glTexCoord2fv(texseg[j]);
+            glVertex3f(seg[j][1], height[1] + beltHeight, seg[j][0]);
+        }
+    }
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void Belt::drawCornerCCW(bool rev) {
+    static const float deltaAngle = 0.1;
+    float _beltMove = rev ? (1 - beltMove) / radius0 / C : beltMove / radius0 / C;
+
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+    glTranslatef(-0.5, 0, -0.5);
+    // frame
+    texture.bindTex(0);
+    glBegin(GL_QUADS);
+    // down
+    for (float i = 0; i < 90; i += deltaAngle) {
+        float rad[] = { i * C, (i + deltaAngle) * C };
+        float seg[][2] = { {radius[0] * cos(rad[0]), radius[0] * sin(rad[0])},
+                           {radius[3] * cos(rad[0]), radius[3] * sin(rad[0])},
+                           {radius[3] * cos(rad[1]), radius[3] * sin(rad[1])},
+                           {radius[0] * cos(rad[1]), radius[0] * sin(rad[1])} };
+        float texseg[][2] = { {radius[0], radius0 * rad[0]},
+                              {radius[3], radius0 * rad[0]},
+                              {radius[3], radius0 * rad[1]},
+                              {radius[0], radius0 * rad[1]} };
+        for (int j = 0; j < 4; j++) {
+            glTexCoord2fv(texseg[j]);
+            glVertex3f(seg[j][1], height[0], 1 - seg[j][0]);
+        }
+    }
+    // up
+    for(int k = 0; k < 3; k++){
+        for (float i = 0; i < 90; i += deltaAngle) {
+            float rad[] = { i * C, (i + deltaAngle) * C };
+            float seg[][2] = { {radius[k] * cos(rad[0]), radius[k] * sin(rad[0])},
+                               {radius[k+1] * cos(rad[0]), radius[k+1] * sin(rad[0])},
+                               {radius[k+1] * cos(rad[1]), radius[k+1] * sin(rad[1])},
+                               {radius[k] * cos(rad[1]), radius[k] * sin(rad[1])} };
+            float texseg[][2] = { {radius[k], radius0 * rad[0]},
+                                  {radius[k+1], radius0 * rad[0]},
+                                  {radius[k+1], radius0 * rad[1]},
+                                  {radius[k], radius0 * rad[1]} };
+            for (int j = 0; j < 4; j++) {
+                glTexCoord2fv(texseg[j]);
+                glVertex3f(seg[j][1], height[k==1 ? 1 : 2], 1 - seg[j][0]);
+            }
+        }
+    }
+    // side
+    for (int k = 0; k < 4; k++) {
+        for (float i = 0; i < 90; i += deltaAngle) {
+            float rad[] = { i * C, (i + deltaAngle) * C };
+            float seg[][2] = { {radius[k] * cos(rad[0]), radius[k] * sin(rad[0])},
+                               {radius[k] * cos(rad[1]), radius[k] * sin(rad[1])} };
+            glTexCoord2f(height[2], radius[k] * rad[0] - int(radius[k] * rad[0])); glVertex3f(seg[0][1], height[2], 1 - seg[0][0]);
+            glTexCoord2f(height[2], radius[k] * rad[1] - int(radius[k] * rad[1])); glVertex3f(seg[1][1], height[2], 1 - seg[1][0]);
+            if (k == 0 || k == 3) {
+                glTexCoord2f(height[0], radius[k] * rad[1] - int(radius[k] * rad[1])); glVertex3f(seg[1][1], height[0], 1 - seg[1][0]);
+                glTexCoord2f(height[0], radius[k] * rad[0] - int(radius[k] * rad[0])); glVertex3f(seg[0][1], height[0], 1 - seg[0][0]);
+            }
+            else {
+                glTexCoord2f(height[1], radius[k] * rad[1] - int(radius[k] * rad[1])); glVertex3f(seg[1][1], height[1], 1 - seg[1][0]);
+                glTexCoord2f(height[1], radius[k] * rad[0] - int(radius[k] * rad[0])); glVertex3f(seg[0][1], height[1], 1 - seg[0][0]);
+            }
+        }
+    }
+    glEnd();
+
+    // belt
+    texture.bindTex(1);
+    glBegin(GL_QUADS);
+    for (float i = 0; i < 90; i += deltaAngle) {
+        float rad[] = { i * C, (i + deltaAngle) * C };
+        float texrad[2];
+        if (i >= _beltMove) texrad[0] = (i - _beltMove) * C, texrad[1] = (i + deltaAngle - _beltMove) * C;
+        else texrad[0] = (i + (90 - _beltMove)) * C, texrad[1] = (i + deltaAngle + (90 - _beltMove)) * C;
+        float seg[][2] = { {radius[1] * cos(rad[0]), radius[1] * sin(rad[0])},
+                           {radius[2] * cos(rad[0]), radius[2] * sin(rad[0])},
+                           {radius[2] * cos(rad[1]), radius[2] * sin(rad[1])},
+                           {radius[1] * cos(rad[1]), radius[1] * sin(rad[1])} };
+        float texseg[][2] = { {radius[2], radius0 * texrad[0]},
+                              {radius[1], radius0 * texrad[0]},
+                              {radius[1], radius0 * texrad[1]},
+                              {radius[2], radius0 * texrad[1]} };
+        for (int j = 0; j < 4; j++) {
+            glTexCoord2fv(texseg[j]);
+            glVertex3f(seg[j][1], height[1] + beltHeight, 1 - seg[j][0]);
+        }
+    }
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
---
+++ project/main.cpp
@@ -0,0 +1,156 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <gl/glut.h>
+#include "belt.h"
+#include "skybox.h"
+#include "camera.h"
+
+int OriX = -1, OriY = -1;
+bool rightDown = 0;
+bool bEdit = false;	// 编辑模式，从y = 100俯视
+
+Belt obj;
+Camera camera;
+SkyBox sky;
+
+void renderScene(void)
+{
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glEnable(GL_LIGHTING);
+
+	GLfloat ambient_color[] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_pos[] = { 5,5,5,1 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_color);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+
+	glEnable(GL_LIGHT0);
+
+	camera.setLook();
+
+	sky.CreateSkyBox(0, 0, 0, 1.0, 0.5, 1.0);
+
+	obj.draw();
+	Belt::update();
+
+	glutSwapBuffers();
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_RIGHT_BUTTON){
+		if(state == GLUT_DOWN){
+			rightDown = 1;
+			OriX = x, OriY = y;
+		}
+		else rightDown = 0;
+	}
+}
+
+void onMouseMove(int x, int y){
+	if(!bEdit)	// 非编辑模式下，视线随鼠标变化
+		camera.setViewByMouse();
+
+	glutPostRedisplay();
+}
+
+void key(unsigned char k, int x, int y){
+	switch (k) {
+	case 27:
+		exit(0);
+		break;
+	case '1':
+		camera.setSpeed(0.2f);
+		break;
+	case '2':
+		camera.setSpeed(1.0f);
+		break;
+	case 'w':
+		camera.moveCamera(camera.getSpeed());
+		break;
+	case 's':
+		camera.moveCamera(-camera.getSpeed());
+		break;
+	case 'a':
+		camera.yawCamera(-camera.getSpeed());
+		break;
+	case 'd':
+		camera.yawCamera(camera.getSpeed());
+		break;
+	case 'e':
+		bEdit = !bEdit;
+		break;
+	}
+
+	glutPostRedisplay();
+	//printf("key::%d", key);
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (float)w / h, 0.1, 4000.0);
+	// glOrtho(-3, 3, -3, 3, -1, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
+}
+
+void init() {
+	Belt::init();
+	obj.pushPoint(-1, 0);
+	obj.pushPoint(0, 0);
+	obj.pushPoint(1, 0);
+	obj.pushPoint(1, 1);
+	obj.pushPoint(2, 1);
+	obj.pushPoint(2, 2);
+	obj.pushPoint(1, 2);
+	obj.pushPoint(1, 3);
+	obj.pushPoint(1, 4);
+	obj.pushPoint(0, 4);
+	obj.pushPoint(0, 3);
+	obj.pushPoint(0, 2);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+	glClearDepth(1.0f);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_DEPTH_TEST);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_TEXTURE_2D);
+
+	if (!sky.Init()){
+		MessageBox(NULL, (LPCWSTR)"Fail to initialize skybox!", (LPCWSTR)"Error", MB_OK);
+		exit(0);
+	}
+
+	camera.setCamera(0, 1, 1, 0, 1, 0, 0, 1, 0);
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1024, 768);
+	glutCreateWindow("CG Project");
+
+	init();
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(renderScene);
+	glutIdleFunc(renderScene);
+	glutMouseFunc(Mouse);
+	glutPassiveMotionFunc(onMouseMove);
+	glutKeyboardFunc(key);
+
+	glutMainLoop();
+	return 0;
+}
